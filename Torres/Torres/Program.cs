1da7cc0986d7547ce572a52daa6de07a650a0c40
Elpoke12 <43417450+Elpoke12@users.noreply.github.com>
2018-11-05 11:29:27 -0800

    Add files via upload

---
+++ Torres/Torres/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Torres
+{
+    class Program
+    {
+        static void TorresHanoi( int o, int n, int v, int aux)
+        {
+            if(n>0)
+            {
+                TorresHanoi(n-1, o, aux, v);
+                Console.WriteLine("Se mueve anillo desde torre"+ o +" hasta torre "+v);
+                TorresHanoi(n-1, aux, v, o);
+            }
+        }
+        static void Main(string[] args)
+        {
+            int n;
+            Console.Write("Favor de ingresar cantidad de anillos: ");
+            n = int.Parse(Console.ReadLine());
+            TorresHanoi(n, 1,3,2);
+            Console.ReadKey();
+        }
+    }
+}
